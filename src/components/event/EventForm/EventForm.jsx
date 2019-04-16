7ea62d9650c9c816394094c041349ad0ff9be13d
Bao Bui <bao.bui@orientsoftware.net>
2019-04-16 15:26:02 +0700

    Section 10

--- src/components/event/EventForm/EventForm.jsx
+++ src/components/event/EventForm/EventForm.jsx
@@ -1,3 +1,4 @@
+/*global google*/
 import React, { Component } from 'react'
 import { Form,Button,Segment, Grid, Header } from 'semantic-ui-react'
 import {connect} from 'react-redux'
@@ -10,8 +11,26 @@ import TextInput from '../../../utils/Form/TextInput'
 import TextArea from '../../../utils/Form/TextArea'
 import SelectInput from '../../../utils/Form/SelectInput'
 import DateInput from '../../../utils/Form/DateInput'
+import PlaceInput from '../../../utils/Form/PlaceInput'
+import Script from 'react-load-script';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
-
+function mapStateToProps(state, ownProps){
+  const eventId = ownProps.match.params.id;
+  let emptyEvent ={}
+      if(eventId && state.eventsReducer.length > 0){
+              emptyEvent = state.eventsReducer.filter(event=>event.id === eventId)[0]
+        }
+  return {
+    initialValues: emptyEvent
+  };
+  }
+  function mapDispatchToProps(dispatch){
+    return{
+        update: (updateevent)=>dispatch(ACTIONS.updateEvent(updateevent)),
+        create: (createevent)=>dispatch(ACTIONS.createEvent(createevent)),
+    }
+}
 const category = [
   {key: 'drinks', text: 'Drinks', value: 'drinks'},
   {key: 'culture', text: 'Culture', value: 'culture'},
@@ -34,8 +53,42 @@ const validate = combineValidators({
 
 })
 class EventForm extends Component {
+  state = {
+    cityLatLng: {},
+    venueLatLng: {},
+    scriptLoaded: false
+  };
+
+  handleScriptLoaded = () => this.setState({ scriptLoaded: true });
+  
+  handleCitySelect = selectedCity => {
+    geocodeByAddress(selectedCity)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => {
+        this.setState({
+          cityLatLng: latlng
+        });
+      })
+      .then(() => {
+        this.props.change('city', selectedCity)
+      })
+  };
+
+  handleVenueSelect = selectedVenue => {
+    geocodeByAddress(selectedVenue)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => {
+        this.setState({
+          venueLatLng: latlng
+        });
+      })
+      .then(() => {
+        this.props.change('venue', selectedVenue)
+      })
+  };
   onFormSubmit = values =>{
     values.date = moment(values.date).format()
+    values.venueLatLng = this.state.venueLatLng;
     if(this.props.initialValues.id){
         this.props.update(values)
         this.props.history.goBack();
@@ -55,6 +108,10 @@ class EventForm extends Component {
     const {invalid, submitting, pristine} = this.props;
     return (
       <Grid>
+         <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMFeSw9ED2eOKpVp13MKhh99F-I2w88gw&libraries=places"
+          onLoad={this.handleScriptLoaded}
+        />
           <Grid.Column width={10}>
           <Segment>
             <Header sub color='teal' content='Event Detailes'/>
@@ -74,16 +131,36 @@ class EventForm extends Component {
                 component={TextArea} 
                 placeholder="Tell us about your event"/>
                 <Header sub color='teal' content='Event Location Details'/>
-                <Field name='city' type='text' component={TextInput} placeholder="Event City"/>
-                <Field name='venue' type='text' component={TextInput} placeholder="Event Venue"/>
+                <Field
+                name="city"
+                type="text"
+                component={PlaceInput}
+                options={{ types: ['(cities)'] }}
+                placeholder="Event city"
+                onSelect={this.handleCitySelect}
+              />
+              {this.state.scriptLoaded &&
+              <Field
+                name="venue"
+                type="text"
+                component={PlaceInput}
+                options={{
+                  location: new google.maps.LatLng(this.state.cityLatLng),
+                  radius: 1000,
+                  types: ['establishment']
+                }}
+                placeholder="Event venue"
+                onSelect={this.handleVenueSelect}
+              />}
                 <Field
                 name="date"
-                type="text"
-                component={DateInput}
-                dateFormat='YYYY-MM-DD HH:mm'
-                timeFormat='HH:mm'
-                showTimeSelect
-                placeholder="Date and time of event"/>            <Button disabled={invalid || submitting || pristine} positive type="submit">
+                type="date"
+                component={TextInput}
+                // dateFormat='YYYY-MM-DD HH:mm'
+                // timeFormat='HH:mm'
+                // showTimeSelect
+                placeholder="Date and time of event"/>           
+               <Button disabled={invalid || submitting || pristine} positive type="submit">
                 Submit
               </Button>
               <Button type="button" onClick={this.props.history.goBack}>Cancel</Button>
@@ -96,20 +173,5 @@ class EventForm extends Component {
   }
 }
 
-function mapStateToProps(state, ownProps){
-  const eventId = ownProps.match.params.id;
-  let emptyEvent ={}
-      if(eventId && state.eventsReducer.length > 0){
-              emptyEvent = state.eventsReducer.filter(event=>event.id === eventId)[0]
-        }
-  return {
-    initialValues: emptyEvent
-  };
-  }
-  function mapDispatchToProps(dispatch){
-    return{
-        update: (updateevent)=>dispatch(ACTIONS.updateEvent(updateevent)),
-        create: (createevent)=>dispatch(ACTIONS.createEvent(createevent)),
-    }
-}
+
 export default connect(mapStateToProps,mapDispatchToProps)(reduxForm({form: 'eventForm', enableReinitialize: true,validate})(EventForm));
